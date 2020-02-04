bd974bb751992f5f55e7c118cc384b8dd6d36151
Brian Goldenberg <bggolden11@gmail.com>
2020-02-03 22:25:07 -0600

    added inital waiter implementation

--- Backend/functions/src/index.ts
+++ Backend/functions/src/index.ts
@@ -1,8 +1,17 @@
 import * as functions from 'firebase-functions';
+import {Waiter} from "./Types/Waiter";
+import { Table } from './Types/Table';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
+let waiter = new Waiter("Jane Doe");
+waiter.addToQueue(new Table("Not a real table",3,3,));
+console.log("Waiter name " + waiter.name + "\n");
+console.log("Table name " + waiter.getNextInQueue().name);
+   
  response.send("Hello from Firebase!");
 });
+
+

1b4c57069b0762d1c7859090551950654a4ed5c6
Brian Goldenberg <bggolden11@gmail.com>
2020-02-07 18:04:35 -0600

    with firebase and azure

--- Backend/functions/src/index.ts
+++ Backend/functions/src/index.ts
@@ -5,13 +5,18 @@ import { Table } from './Types/Table';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
+
 let waiter = new Waiter("Jane Doe");
 waiter.addToQueue(new Table("Not a real table",3,3,));
+
+export const getWaiterName = functions.https.onRequest((request, response) => {
+    response.send("THE new Waiter name " + waiter.name + "\n");
+});
+
+export const helloWorld = functions.https.onRequest((request, response) => {
 console.log("Waiter name " + waiter.name + "\n");
-console.log("Table name " + waiter.getNextInQueue().name);
-   
- response.send("Hello from Firebase!");
+console.log("Table name " + waiter.getNextInQueue().name); 
+ response.send("waiter.getNextInQueue().name);
 });
 
 

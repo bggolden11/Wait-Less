9c9a1f8e849edf9c2618eef9752a0b836dc5f8c7
hdpgupta <60522435+hdpgupta@users.noreply.github.com>
2020-02-15 13:15:21 -0600

    Update main.dart

--- Frontend/wait_less/lib/main.dart
+++ Frontend/wait_less/lib/main.dart
@@ -1,44 +1,155 @@
 import 'package:flutter/material.dart';
 
-void main()=> runApp(new MyApp());
+main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return new MaterialApp(
-        home: new LoginPage(),
-        theme: new ThemeData(
-            primarySwatch: Colors.lightGreen
-        )
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Screen',
+      home: LoginPage(),
     );
   }
 }
 
-class LoginPage extends StatefulWidget{
+class LoginPage extends StatefulWidget {
   @override
-  State createState() => new LoginPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class LoginPageState extends State<LoginPage>{
+class _LoginPageState extends State<LoginPage> {
   @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      backgroundColor: Colors.black,
-      body: new Stack(
-        children: <Widget>[
-          new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new Image(
-                  image: new AssetImage("image/logo.jpg"),
-                  fit: BoxFit.cover,
-//          color: Colors.black54,
-//          colorBlendMode:BlendMode.darken,
-                )
-              ]
-          )
-        ],
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomPaint(
+        painter: CurvePainter(),
+        child: _buildLoginLayout(),
       ),
     );
+  }
+
+  Widget _buildLoginLayout() {
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        new Image(
+          image: new AssetImage("image/logo.jpg"),
+          fit: BoxFit.cover,
+          color: Colors.black87,
+          colorBlendMode: BlendMode.darken,
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: AppLogo(),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: _buildLoginFields(),
+        )
+      ],
+    );
+  }
+
+  Widget _buildLoginFields() {
+    return Container(
+      margin: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 80.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _buildEmailField(),
+            SizedBox(
+              height: 10.0,
+            ),
+            _buildPasswordField(),
+            SizedBox(
+              height: 20.0,
+            ),
+            _buildSubmitButton(),
+            SizedBox(
+              height: 10.0,
+            ),
+            _buildRegisterText(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmailField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Username'),
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Password'),
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return RaisedButton(
+      onPressed: () {},
+      color: Colors.blueAccent[200],
+      child: Text(
+        'LOGIN',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+
+  Widget _buildRegisterText() {
+    return Text(
+      'REGISTER',
+      textAlign: TextAlign.center,
+      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+    );
+  }
+}
+
+class AppLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 50.0),
+      width: 100.0,
+      height: 100.0,
+      decoration:
+      BoxDecoration(shape: BoxShape.circle, color: Colors.grey[800]),
+      child: Icon(
+        Icons.home,
+        color: Colors.white,
+        size: 40.0,
+      ),
+    );
+  }
+}
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.green[800];
+    paint.style = PaintingStyle.fill;
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.875,
+        size.width * 0.5, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.9584,
+        size.width * 1.0, size.height * 0.9167);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }

adf46ed204c55f07a14dcc45cee3974c4dd498ae
Brian Goldenberg <bggolden11@gmail.com>
2020-02-19 10:44:15 -0600

    renaming files

--- Code/Backend/waitless-functions/src/main/java/DBO/Queries/AuthenticatingUserDBOQuery.java
+++ Code/Backend/waitless-functions/src/main/java/DBO/Queries/AuthenticatingUserDBOQuery.java
@@ -5,14 +5,8 @@ import Exceptions.UserNotFoundException;
 
 import java.sql.*;
 
-public class AuthenticatingUserDBOQuery {
+public class AuthenticatingUserDBOQuery implements DBO {
 
-    String hostName = "waitless.database.windows.net"; // update me
-    String dbName = "WaitlessDB"; // update me
-    String user = "wladmin"; // update me
-    String password = "Waitlesspassword123"; // update me
-    String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
-            + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
     Connection connection = null;
 
     public UserAuthenticationDBO userAuthenticate(String username) throws SQLException, UserNotFoundException {
@@ -20,9 +14,6 @@ public class AuthenticatingUserDBOQuery {
         connection = DriverManager.getConnection(url);
         String schema = connection.getSchema();
         System.out.println("Successful connection - Schema: " + schema);
-
-        System.out.println("Query data example:");
-        System.out.println("=========================================");
 
         // Create and execute a SELECT SQL statement.
         String selectSql = "SELECT * "

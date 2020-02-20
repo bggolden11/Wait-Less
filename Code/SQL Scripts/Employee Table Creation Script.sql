b00341d03e87ad67104fda3cbd00b79379db9a5a
Emmanuel Utomi <eutomi2@uic.edu>
2020-02-20 01:01:26 -0600

    SQLDB: Change employee position to isManager
    This will display a boolean, that can be easily checked to grant a user admin priviledges in frontend

--- Code/SQL Scripts/Employee Table Creation Script.sql
+++ Code/SQL Scripts/Employee Table Creation Script.sql
@@ -12,7 +12,7 @@ CREATE TABLE [dbo].[Employee](
 	[Employee_ID] [int] NOT NULL IDENTITY(2121,1) PRIMARY KEY,
 	[FName] [varchar](50) NOT NULL,
 	[LName] [varchar](50) NOT NULL,
-	[Position] [varchar](50) NOT NULL,
+	[isManager] [int] NOT NULL,
 	[Hire_Date] [date] NOT NULL,
 	[Birth_Date] [date] NOT NULL,
 	[Address] [varchar](50) NOT NULL,
@@ -22,21 +22,20 @@ CREATE TABLE [dbo].[Employee](
 )
 GO
 
-INSERT INTO [dbo].[Employee] 
-	( [FName], [LName], [Position], [Hire_Date],
+INSERT INTO [dbo].[Employee]
+	( [FName], [LName], [isManager], [Hire_Date],
       [Birth_Date], [Address], [Phone], [Token], [Salary] )
-VALUES ('Manager', 'Test', 'Manager', '2001-11-11', '2001-12-12', '123 Drive', '123-456-7890', 'jAl4N4o8EGTKM0DLJiblQQ==', 14.20)
+VALUES ('Manager', 'Test', '1', '2001-11-11', '2001-12-12', '123 Drive', '123-456-7890', 'jAl4N4o8EGTKM0DLJiblQQ==', 14.20)
 GO
 
-INSERT INTO [dbo].[Employee] 
-	( [FName], [LName], [Position], [Hire_Date],
+INSERT INTO [dbo].[Employee]
+	( [FName], [LName], [isManager], [Hire_Date],
       [Birth_Date], [Address], [Phone], [Token], [Salary] )
-VALUES ('Host', 'Test', 'Host', '2001-11-11', '2001-12-12', '4389 Circle', '928-456-2354', 'jAl4N4o8EGTKM0DLJiblQQ==', 8.80)
+VALUES ('Host', 'Test', '0', '2001-11-11', '2001-12-12', '4389 Circle', '928-456-2354', 'jAl4N4o8EGTKM0DLJiblQQ==', 8.80)
 GO
 
-INSERT INTO [dbo].[Employee] 
-	( [FName], [LName], [Position], [Hire_Date],
+INSERT INTO [dbo].[Employee]
+	( [FName], [LName], [isManager], [Hire_Date],
       [Birth_Date], [Address], [Phone], [Token], [Salary] )
-VALUES ('Waiter', 'Test', 'Waiter', '2001-11-11', '2001-12-12', '390 Ave', '347-456-7890', 'jAl4N4o8EGTKM0DLJiblQQ==', 10.0)
+VALUES ('Waiter', 'Test', '0', '2001-11-11', '2001-12-12', '390 Ave', '347-456-7890', 'jAl4N4o8EGTKM0DLJiblQQ==', 10.0)
 GO
-

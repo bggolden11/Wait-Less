d29731431fbce668ee47bb39eef3f26e4d585310
hgupta20 <harshdevprakashgupta@outlook.com>
2020-02-23 16:13:06 -0600

    registration screen state

---
+++ Code/Frontend/wait_less/lib/src/screens/registration_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatelessWidget {
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}

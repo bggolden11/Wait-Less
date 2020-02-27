e52bbf13b9ef1f6241e399f624b7e583383798ef
Brian Goldenberg <bggolden11@gmail.com>
2020-02-27 12:12:33 -0600

    changes create User requests

---
+++ Code/Backend/waitless-functions/src/main/java/Response/CreateUserResponse.java
@@ -0,0 +1,8 @@
+package Response;
+
+public class CreateUserResponse {
+    String temporaryPassword;
+    public CreateUserResponse(String temporaryPassword){
+        this.temporaryPassword = temporaryPassword;
+    }
+}

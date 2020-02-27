1e173785a8334967715b5f4cf7a1b4913cddd607
Brian Goldenberg <bggolden11@gmail.com>
2020-02-27 12:57:38 -0600

    added employeeId in response Body

--- Code/Backend/waitless-functions/src/main/java/Response/CreateUserResponse.java
+++ Code/Backend/waitless-functions/src/main/java/Response/CreateUserResponse.java
@@ -1,8 +1,11 @@
 package Response;
 
 public class CreateUserResponse {
+    String employeeId;
     String temporaryPassword;
-    public CreateUserResponse(String temporaryPassword){
+
+    public CreateUserResponse(String employeeId, String temporaryPassword){
+        this.employeeId = employeeId;
         this.temporaryPassword = temporaryPassword;
     }
 }
--- Code/Backend/waitless-functions/src/main/java/Service/CreateUser.java
+++ Code/Backend/waitless-functions/src/main/java/Service/CreateUser.java
@@ -3,6 +3,7 @@ package Service;
 
 import Encryption.AES;
 import DBO.Queries.CreateUserDBOQuery;
+import Exceptions.UserNotFoundException;
 import Requests.CreateUserRequest;
 import Response.CreateUserResponse;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -16,15 +17,11 @@ import java.util.Random;
 public class CreateUser {
 
     public HttpResponseMessage create(HttpRequestMessage<Optional<CreateUserRequest>> request, String firstName, String lastName, String birthday, String address, String phone, String title) {
-        Random rnd = new Random();
-        String temporaryPassword = String.format("%06d",rnd.nextInt(9999));
-        String encryptedPassword = (new AES()).encrypt(temporaryPassword);
+        String initialPassword = String.format("%06d",(new Random()).nextInt(9999));
+        String encryptedPassword = (new AES()).encrypt(initialPassword);
         try {
-            if (new CreateUserDBOQuery().create(firstName, lastName, title.equals("Manager") ? 1 : 0, birthday, address, phone, 10.0, encryptedPassword, title)) {
-                return request.createResponseBuilder(HttpStatus.OK).body(new CreateUserResponse(temporaryPassword)).build();
-            } else {
-                return request.createResponseBuilder(HttpStatus.UNAUTHORIZED).body("New waiter was not created").build();
-            }
+                String employeeID = new CreateUserDBOQuery().create(firstName, lastName, title.equals("Manager") ? 1 : 0, birthday, address, phone, 10.0, encryptedPassword, title);
+                return request.createResponseBuilder(HttpStatus.OK).body(new CreateUserResponse(employeeID,initialPassword)).build();
         }
         catch (SQLException e){
             return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Error connecting to SQL database").build();

baf57eebd3f7cd049e20a5a90034926d596fc365
Brian Goldenberg <33329982+bggolden11@users.noreply.github.com>
2020-03-09 23:59:42 -0500

    Change azure stuff (#27)
    * Change azure function thing
    
    * Change azure function thing

--- Code/Backend/waitless-functions/src/main/java/DBO/Queries/DBO.java
+++ Code/Backend/waitless-functions/src/main/java/DBO/Queries/DBO.java
@@ -1,10 +1,10 @@
 package DBO.Queries;
 
 public interface DBO {
-    String hostName = "waitless.database.windows.net"; // update me
-    String dbName = "WaitlessDB"; // update me
-    String user = "wladmin"; // update me
-    String password = "Waitlesspassword123"; // update me
+    String hostName = "waitlessserver.database.windows.net";
+    String dbName = "WaitlessDB";
+    String user = "wladmin";
+    String password = "Waitlesspassword123";
     String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
             + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
 }

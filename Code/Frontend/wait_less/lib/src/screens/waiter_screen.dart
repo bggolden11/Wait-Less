88198820e169e87c73d624a56a4b5d0e5bda756a
hgupta20 <harshdevprakashgupta@outlook.com>
2020-03-12 04:11:50 -0500

    Waiter Completed Task UI changes

--- Code/Frontend/wait_less/lib/src/screens/waiter_screen.dart
+++ Code/Frontend/wait_less/lib/src/screens/waiter_screen.dart
@@ -10,11 +10,12 @@ class _WaiterPage extends State<WaiterPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[600],
+      backgroundColor: Colors.white,
       appBar: AppBar( // to edit anything in the appbar
         backgroundColor: Colors.transparent, // no background color
-        elevation: 0, // provides the appbar with 0 elevation to remove the shadow
-        title: new Center(child: new Text("Wait Less", style: TextStyle(color: Colors.white, fontFamily: "Poppins-Bold"))), // center the name of the app
+        iconTheme: new IconThemeData(color: Colors.black),
+        elevation: 90, // provides the appbar with 40 elevation to remove the shadow
+        title: new Center(child: new Text("Wait Less", style: TextStyle(color: Colors.black, fontFamily: "Poppins-Bold"))), // center the name of the app
         //leading: Icon(Icons.menu),
 
         actions: <Widget>[ // for the menu and the notifications center
@@ -38,10 +39,10 @@ class _WaiterPage extends State<WaiterPage>{
           children: <Widget>[ // Drawer class for the navbar and edit User Header if needed to change anything on the User Header Section (Image)
             new UserAccountsDrawerHeader(accountName: new Text("Harsh Gupta", style: TextStyle(color: Colors.white, fontSize: 18.0, fontWeight: FontWeight.bold, fontFamily: "Poppins-Medium")), accountEmail: new Text("1024023", style: TextStyle(color: Colors.white, fontSize: 15.0, fontFamily: "Poppins-Medium",)), decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage("assets/waiter.jpg"),
+                    image: AssetImage("assets/task4.jpg"),
 
                     fit: BoxFit.cover,
-                    colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.7), BlendMode.darken)
+                    colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken)
                 )
             ),
             ),
@@ -77,75 +78,75 @@ class _WaiterPage extends State<WaiterPage>{
       body: SafeArea( // this is the main body and has bunch of containers
         //fit: StackFit.expand,
         child: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
 
-                    new Align(alignment: Alignment.centerLeft, child: new Text(
-                      'Your Tasks', // for the area where the current tasks would be developed
-                      style:TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white, fontFamily: "Poppins-Medium"),
-                    )),
-                  ],
-                ),
-                Padding( // provide padding to create some space between the title and the buttons
-                  padding: EdgeInsets.only(top: 20.0),
+                      new Align(alignment: Alignment.centerLeft, child: new Text(
+                        'Your Tasks', // for the area where the current tasks would be developed
+                        style:TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: "Poppins-Medium"),
+                      )),
+                    ],
+                  ),
+                  Padding( // provide padding to create some space between the title and the buttons
+                    padding: EdgeInsets.only(top: 20.0),
 
-                ),
+                  ),
 
-                Container( // this button is for completed tasks
-                  width: double.infinity,
-                  height: 180,
+                  Container( // this button is for completed tasks
+                    width: double.infinity,
+                    height: 180,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                        image: AssetImage("assets/image_04.png"), // insert the image here
-                        fit: BoxFit.cover
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            image: AssetImage("assets/ctasks.jpg"), // insert the image here
+                            fit: BoxFit.cover
 
-                      )
+                        )
                     ),
                     child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomRight,
-                          colors: [
-                            Colors.black.withOpacity(0.7), // adding opacity in order to increase visibility
-                            Colors.black.withOpacity(0.5),
-                          ]
-                        )
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: LinearGradient(
+                                begin: Alignment.bottomRight,
+                                colors: [
+                                  Colors.black.withOpacity(0.4), // adding opacity in order to increase visibility
+                                  Colors.black.withOpacity(0.2),
+                                ]
+                            )
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
 //                          Text(
 //                            'Your Tasks', // for the area where the current tasks would be developed
 //                            style:TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white, fontFamily: "Poppins-Medium"),
 //
 //                          ),
-                          SizedBox(height: 10,),
-                          Container(
-                            height: 50,
-                            margin: EdgeInsets.symmetric(horizontal: 40),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white
+                            SizedBox(height: 10,),
+                            Container(
+                              height: 50,
+                              margin: EdgeInsets.symmetric(horizontal: 40),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.redAccent[200]
+                              ),
+                              child: Center(child: Text("Completed Tasks", style: TextStyle(color: Colors.grey[900], fontWeight: FontWeight.bold),)),
                             ),
-                            child: Center(child: Text("Completed Tasks", style: TextStyle(color: Colors.grey[900], fontWeight: FontWeight.bold),)),
-                          ),
-                          SizedBox(height: 20,),
-                        ],
-                      )
+                            SizedBox(height: 20,),
+                          ],
+                        )
                     ),
-                )
-              ],
-            ),
+                  )
+                ],
+              ),
 
 
-          )
+            )
 
 
 
@@ -156,4 +157,3 @@ class _WaiterPage extends State<WaiterPage>{
     );
   }
 }
-

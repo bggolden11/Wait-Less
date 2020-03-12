69c741316d0e0dbb966a721cfa098cf402fed451
hgupta20 <harshdevprakashgupta@outlook.com>
2020-03-12 02:35:52 -0500

    Waiter Completed Tasks

--- Code/Frontend/wait_less/lib/src/screens/waiter_screen.dart
+++ Code/Frontend/wait_less/lib/src/screens/waiter_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -12,41 +11,43 @@ class _WaiterPage extends State<WaiterPage>{
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[600],
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
+      appBar: AppBar( // to edit anything in the appbar
+        backgroundColor: Colors.transparent, // no background color
+        elevation: 0, // provides the appbar with 0 elevation to remove the shadow
+        title: new Center(child: new Text("Wait Less", style: TextStyle(color: Colors.white, fontFamily: "Poppins-Bold"))), // center the name of the app
         //leading: Icon(Icons.menu),
 
-        actions: <Widget>[
+        actions: <Widget>[ // for the menu and the notifications center
           Padding(
             padding: EdgeInsets.all(10.0),
             child: Container(
               width: 36,
               height: 30,
               decoration: BoxDecoration(
-                color: Colors.amber,
-                borderRadius: BorderRadius.circular(10)
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(10)
               ),
-              child: Center(child: Text("1")),
+              child: Center(child: Text("1")), // once notifications are implemented we can increase and decrease the counter accordingly and implement this
             ),
           )
 
         ],
       ),
-      drawer: new Drawer(
+      drawer: new Drawer( // this is for the navBar or the sideBar, don't edit unless you want to edit something on the menu
         child: new ListView(
           children: <Widget>[ // Drawer class for the navbar and edit User Header if needed to change anything on the User Header Section (Image)
-            new UserAccountsDrawerHeader(accountName: new Text("Harsh Gupta", style: TextStyle(color: Colors.black87)), accountEmail: new Text("1024023", style: TextStyle(color: Colors.black87)), decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/waiter.jpg"),
-                fit: BoxFit.cover,
-                  colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.dstATop)
-              )
+            new UserAccountsDrawerHeader(accountName: new Text("Harsh Gupta", style: TextStyle(color: Colors.white, fontSize: 18.0, fontWeight: FontWeight.bold, fontFamily: "Poppins-Medium")), accountEmail: new Text("1024023", style: TextStyle(color: Colors.white, fontSize: 15.0, fontFamily: "Poppins-Medium",)), decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/waiter.jpg"),
+
+                    fit: BoxFit.cover,
+                    colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.7), BlendMode.darken)
+                )
             ),
             ),
 
-            new Divider(),
-            new ListTile(
+            new Divider(), // like a padding divides the image with the options in the menu
+            new ListTile( // different options and their icons from Icons package
               title: new Text("Home"),
               trailing: new Icon(Icons.home),
             ),
@@ -73,48 +74,86 @@ class _WaiterPage extends State<WaiterPage>{
           ],
         ),
       ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Expanded(
-                child: Container(),
-              ),
-              Image.asset("assets/image_02.png")
-            ],
-          ),
+      body: SafeArea( // this is the main body and has bunch of containers
+        //fit: StackFit.expand,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
 
-        ],
+                    new Align(alignment: Alignment.centerLeft, child: new Text(
+                      'Your Tasks', // for the area where the current tasks would be developed
+                      style:TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white, fontFamily: "Poppins-Medium"),
+                    )),
+                  ],
+                ),
+                Padding( // provide padding to create some space between the title and the buttons
+                  padding: EdgeInsets.only(top: 20.0),
+
+                ),
+
+                Container( // this button is for completed tasks
+                  width: double.infinity,
+                  height: 180,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                        image: AssetImage("assets/image_04.png"), // insert the image here
+                        fit: BoxFit.cover
+
+                      )
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomRight,
+                          colors: [
+                            Colors.black.withOpacity(0.7), // adding opacity in order to increase visibility
+                            Colors.black.withOpacity(0.5),
+                          ]
+                        )
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+//                          Text(
+//                            'Your Tasks', // for the area where the current tasks would be developed
+//                            style:TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white, fontFamily: "Poppins-Medium"),
+//
+//                          ),
+                          SizedBox(height: 10,),
+                          Container(
+                            height: 50,
+                            margin: EdgeInsets.symmetric(horizontal: 40),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white
+                            ),
+                            child: Center(child: Text("Completed Tasks", style: TextStyle(color: Colors.grey[900], fontWeight: FontWeight.bold),)),
+                          ),
+                          SizedBox(height: 20,),
+                        ],
+                      )
+                    ),
+                )
+              ],
+            ),
+
+
+          )
+
+
+
+        ),
+
       ),
 
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 

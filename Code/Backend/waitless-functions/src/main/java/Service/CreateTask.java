4c78a894ffd093d518aeeda7fb33d378e4401ffb
Brian Goldenberg <bggolden11@gmail.com>
2020-03-13 22:12:34 -0500

    :hammer: added create task api

---
+++ Code/Backend/waitless-functions/src/main/java/Service/CreateTask.java
@@ -0,0 +1,37 @@
+package Service;
+
+import DBO.Queries.CreateTaskDBOQuery;
+import DBO.Queries.GetEmployeeDBOQuery;
+import Exceptions.UserNotFoundException;
+import Requests.CreateTaskRequest;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class CreateTask {
+
+    /**
+     *
+     * @param request request for create task request
+     * @param employeeId employeeId of the user to add the task to
+     * @param status Status of the task
+     * @param message Message of the task
+     * @return 201 if task successfully created
+     *         404 if employeeID not found
+     *         500 SQL error
+     */
+
+    public HttpResponseMessage create(HttpRequestMessage<Optional<CreateTaskRequest>> request, String employeeId, String status, String message){
+        try{
+            (new GetEmployeeDBOQuery()).getEmployee(employeeId);
+            return request.createResponseBuilder(HttpStatus.CREATED).body(new CreateTaskDBOQuery().createTask(employeeId,status, message)).build();
+        } catch (UserNotFoundException e) {
+            return request.createResponseBuilder(HttpStatus.NOT_FOUND).body("Could not find user").build();
+        } catch (SQLException e) {
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Error connecting to SQL database").build();
+        }
+    }
+}
--- Code/Backend/waitless-functions/src/main/java/com/waitless/functions/Function.java
+++ Code/Backend/waitless-functions/src/main/java/com/waitless/functions/Function.java
@@ -4,9 +4,11 @@ import java.sql.SQLException;
 import java.util.*;
 
 import Exceptions.UserNotFoundException;
+import Requests.CreateTaskRequest;
 import Requests.CreateUserRequest;
 import Requests.GetEmployeeRequest;
 import Requests.UserAuthenticationRequest;
+import Service.CreateTask;
 import Service.GetEmployee;
 import Service.UserAuthentication;
 import Service.CreateUser;
@@ -36,74 +38,6 @@ public class Function {
             return request.createResponseBuilder(HttpStatus.OK).body("Harsh is very pretty " + name).build();
         }
     }
-
-    /** needs to be rewritten for sql
-    @FunctionName("GetWaiter")
-    public HttpResponseMessage getWaiter(@HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
-                                         final ExecutionContext context) {
-        ObjectMapper mapper = new ObjectMapper();
-        String query = request.getQueryParameters().get("name");
-        String name = request.getBody().orElse(query);
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the waiter name in the request body").build();
-        } else if (waiterInstance != null) {
-            try{
-                Waiter getWaiter = waiterInstance.getWaiter(name);
-                if(getWaiter == null)
-                    return request.createResponseBuilder(HttpStatus.NOT_FOUND).body("Waiter not found").build();
-                else
-                        return request.createResponseBuilder(HttpStatus.OK).body(mapper.writeValueAsString(getWaiter)).build();
-            }
-            catch(Exception e){
-                context.getLogger().info("Throwing exception \n " + e.getStackTrace());
-                return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("parsing json error").build();
-            }
-        } else {
-            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("getting the singleton failed").build();
-        }
-    }
-
-    @FunctionName("AddTableForWaiter")
-    public HttpResponseMessage addTableForWaiter(@HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<AddNewTableRequest>> request,
-                                                final ExecutionContext context) {
-        String query = request.getQueryParameters().get("name");
-        AddNewTableRequest addNewTableRequest = request.getBody().orElse(null);
-
-        if(addNewTableRequest!= null) {
-            if (waiterInstance != null) {
-                if (waiterInstance.addTable(addNewTableRequest.waiterName, addNewTableRequest.tableNumber)) {
-                    return request.createResponseBuilder(HttpStatus.OK).body("Table successfully added").build();
-                } else {
-                    return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Adding table failed").build();
-                }
-            }
-            else{
-                return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Getting singleton failed ").build();
-            }
-        }
-        else{
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please input a valid waiter name and table number").build();
-            }
-    }
-
-    @FunctionName("AddWaiter")
-    public HttpResponseMessage addWaiter(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
-        //String query = request.getQueryParameters().get("name");
-
-        String name = request.getBody().orElse(query);
-        CreateNewUserRequest createUserRequest = request.getBody().orElse(null);
-
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the waiter name in the request body").build();
-        } else if (waiterInstance != null) {
-                waiterInstance.addWaiter((new Waiter(name)));
-                return request.createResponseBuilder(HttpStatus.OK).body("Waiter added succesfully").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("getting the singleton failed").build();
-        }
-    } **/
 
     @FunctionName("Add-User")
     public HttpResponseMessage AddUser(@HttpTrigger(name = "req", methods = {HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<CreateUserRequest>> request,
@@ -141,6 +75,17 @@ public class Function {
         }
         else{
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please input a valid employeeId").build();
+        }
+    }
+
+    @FunctionName("Create-Task")
+    public HttpResponseMessage createTask(@HttpTrigger(name = "req", methods = {HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<CreateTaskRequest>> request,
+                                          final ExecutionContext context){
+        CreateTaskRequest createTaskRequest = request.getBody().orElse(null);
+        if(createTaskRequest != null)
+            return (new CreateTask()).create(request,createTaskRequest.employeeId,createTaskRequest.status,createTaskRequest.message);
+        else{
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please input a valid username and password").build();
         }
     }
 }

c2d1f3510e17f606f9490a54c2ae86372877a8b7
Brian Goldenberg <33329982+bggolden11@users.noreply.github.com>
2020-03-25 17:04:29 -0500

    added config file and addition to .gitignore (#61)
    * added config file and addition to .gitignore
    
    * minor changes

--- Code/Backend/waitless-functions/src/main/java/DBO/Queries/DBO.java
+++ Code/Backend/waitless-functions/src/main/java/DBO/Queries/DBO.java
@@ -1,10 +1,17 @@
 package DBO.Queries;
+import com.typesafe.config.*;
 
+/**
+ * DBO interface
+ * Stores database settings
+ */
 public interface DBO {
-    String hostName = "waitlessserver.database.windows.net";
-    String dbName = "WaitlessDB";
-    String user = "wladmin";
-    String password = "Waitlesspassword123";
+    Config conf = ConfigFactory.load();
+
+    String hostName = conf.getString("DBO-config.Host_Name");
+    String dbName =  conf.getString("DBO-config.DB_Name");
+    String user = conf.getString("DBO-config.Username");
+    String password = conf.getString("DBO-config.Password");
     String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
             + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
 

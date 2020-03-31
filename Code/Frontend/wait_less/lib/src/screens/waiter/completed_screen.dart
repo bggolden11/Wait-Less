e45155215e2fb8fd72d2bfba54b2ecd125b41944
Omar Salas <osalas3@uic.edu>
2020-03-31 15:50:38 -0500

    Modified loading animation

--- Code/Frontend/wait_less/lib/src/screens/waiter/completed_screen.dart
+++ Code/Frontend/wait_less/lib/src/screens/waiter/completed_screen.dart
@@ -56,7 +56,7 @@ Widget _buildCompletedList() {
           return CompletedList(task, content);
         },
       )
-          : new Center(child: SpinKitWave(color: Colors.lightGreen, size: 100)); //  Haven't gotten the tasks
+          : new Center(child: SpinKitWave(color: Colors.cyan, size: 50)); //  Haven't gotten the tasks
 
     },
 

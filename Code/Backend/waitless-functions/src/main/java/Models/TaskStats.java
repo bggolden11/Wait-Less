7fb4367e3ca0b5d44d12681eda361d196845f55b
Brian Goldenberg <33329982+bggolden11@users.noreply.github.com>
2020-04-06 13:23:11 -0500

    Change pom and fix for get stats (#73)

--- Code/Backend/waitless-functions/src/main/java/Models/TaskStats.java
+++ Code/Backend/waitless-functions/src/main/java/Models/TaskStats.java
@@ -2,21 +2,23 @@ package Models;
 
 import Exceptions.ObtainingAverageException;
 
-import java.sql.Date;
 import java.sql.Time;
 import java.util.OptionalDouble;
 
 public class TaskStats {
     public String date;
     public int numberOfCompletedTasks;
-    public String averageTimeTaken;
+    public Double averageTimeTaken;
 
     public TaskStats(String date, int numberOfCompletedTasks, OptionalDouble averageTimeTaken) throws ObtainingAverageException {
         this.date = date;
         this.numberOfCompletedTasks = numberOfCompletedTasks;
-        if(averageTimeTaken.isPresent())
-            this.averageTimeTaken = (new Time((long)averageTimeTaken.getAsDouble())).toString();
-        else
-            throw new ObtainingAverageException();
+    if (averageTimeTaken.isPresent())
+      this.averageTimeTaken =
+          Double.valueOf(new Time((long) averageTimeTaken.getAsDouble())
+              .toString()
+              .substring(3)
+              .replace(":", "."));
+    else throw new ObtainingAverageException();
     }
 }
